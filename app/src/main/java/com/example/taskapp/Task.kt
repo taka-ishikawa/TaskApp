cc56dc0a46471a8ef46a098035325f84f6441dbf
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-04-18 15:20:33 +0900

    -added "category" field on the model class: Task
    it helps narrow the list

--- app/src/main/java/com/example/taskapp/Task.kt
+++ app/src/main/java/com/example/taskapp/Task.kt
@@ -6,6 +6,7 @@ import java.io.Serializable
 import java.util.*
 
 open class Task: RealmObject(), Serializable {
+    var category: String = ""
     var title: String = ""
     var contents: String = ""
     var date: Date = Date()
