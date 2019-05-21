2f8bb4e2e2ce3cf6b41efbbdd41a0506d772288c
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-05-21 11:11:05 +0900

    [update?] maybe you can make Category class in CategoryActivity

---
+++ app/src/main/java/com/example/taskapp/CategoryActivity.kt
@@ -0,0 +1,50 @@
+package com.example.taskapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_category.*
+import kotlinx.android.synthetic.main.content_category.*
+import kotlinx.android.synthetic.main.content_category.add_category_button
+import kotlinx.android.synthetic.main.content_edit.*
+import java.util.*
+
+class CategoryActivity : AppCompatActivity() {
+
+    private lateinit var category: Category
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+
+        setSupportActionBar(toolbar2)
+
+        title = "カテゴリの追加"
+
+        add_category_button.setOnClickListener {
+            val realm = Realm.getDefaultInstance()
+            realm.beginTransaction()
+
+            val category = Category()
+
+            val categoryRealmResults = realm.where(Category::class.java).findAll()
+            val identifier: Int =
+                if (categoryRealmResults.max("id") != null) { //there are some results
+                    categoryRealmResults.max("id")!!.toInt() + 1
+                } else { //there is no result
+                    0
+                }
+
+            category.strCategory = category_category_edit_text.text.toString()
+            category.categoryId = identifier
+
+            realm.copyToRealmOrUpdate(category)
+            realm.commitTransaction()
+            realm.close()
+
+            Toast.makeText(this, "カテゴリを追加しました", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+}
