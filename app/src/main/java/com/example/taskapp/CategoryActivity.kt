7479819ec4db935a3416c3e2019bbeec1ae4a5f5
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-05-22 12:04:41 +0900

    [add] finished app base function and 1. below.
    some function remained:
    1. the same category cannot be registered.
    2. in EditActivity, set the same category of selected task.
    3. set back button(?): ← on appbar in CategoryActivity.

--- app/src/main/java/com/example/taskapp/CategoryActivity.kt
+++ app/src/main/java/com/example/taskapp/CategoryActivity.kt
@@ -2,6 +2,7 @@ package com.example.taskapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.widget.Toast
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_category.*
@@ -12,7 +13,7 @@ import java.util.*
 
 class CategoryActivity : AppCompatActivity() {
 
-    private lateinit var category: Category
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,29 +21,43 @@ class CategoryActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar2)
 
+        realm = Realm.getDefaultInstance()
+
         add_category_button.setOnClickListener {
-            val realm = Realm.getDefaultInstance()
-            realm.beginTransaction()
+            if (category_category_edit_text.text.toString() != "") {
+                val category = Category()
 
-            val category = Category()
+                val categoryRealmResults = realm.where(Category::class.java).findAll()
+                val identifier: Int =
+                    if (categoryRealmResults.max("categoryId") != null) { //there are some results
+                        categoryRealmResults.max("categoryId")!!.toInt() + 1
+                    } else { //there is no result
+                        0
+                    }
 
-            val categoryRealmResults = realm.where(Category::class.java).findAll()
-            val identifier: Int =
-                if (categoryRealmResults.max("categoryId") != null) { //there are some results
-                    categoryRealmResults.max("categoryId")!!.toInt() + 1
-                } else { //there is no result
-                    0
+                val categoryExistOrNot = realm.where(Category::class.java)
+                    .equalTo("strCategory", category_category_edit_text.text.toString()).findFirst()
+                if (categoryExistOrNot == null) { // category written is not exist
+                    category.strCategory = category_category_edit_text.text.toString()
+                    category.categoryId = identifier
+
+                    realm.beginTransaction()
+                    realm.copyToRealmOrUpdate(category)
+                    realm.commitTransaction()
+
+                    Toast.makeText(this, "カテゴリを追加しました", Toast.LENGTH_SHORT).show()
+                    finish()
+                } else { // category written has been already registered
+                    Snackbar.make(it, "このカテゴリは登録済みです", Snackbar.LENGTH_LONG).show()
+                    return@setOnClickListener
                 }
-
-            category.strCategory = category_category_edit_text.text.toString()
-            category.categoryId = identifier
-
-            realm.copyToRealmOrUpdate(category)
-            realm.commitTransaction()
-            realm.close()
-
-            Toast.makeText(this, "カテゴリを追加しました", Toast.LENGTH_SHORT).show()
-            finish()
+            } else Snackbar.make(it, "カテゴリを入力してください", Snackbar.LENGTH_LONG).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        realm.close()
     }
 }
