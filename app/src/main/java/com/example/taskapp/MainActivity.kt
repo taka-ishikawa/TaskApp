7a4ffa32661d72f8baf8933c06adfc5fd2759531
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-05-22 11:16:15 +0900

    [update] finished app base function.
    some function remained:
    1. the same category cannot be registered.
    2. in EditActivity, set the same category of selected task.
    3. set back button(?): ← on appbar in CategoryActivity.

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -7,9 +7,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import io.realm.Realm
 import io.realm.RealmChangeListener
+import io.realm.Sort
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -21,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     private val mRealmListener = RealmChangeListener<Realm> { reloadListView() }
 
     private lateinit var mTaskAdapter: TaskAdapter
+
+    private var categorySelected: Category? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,28 +43,18 @@ class MainActivity : AppCompatActivity() {
         mTaskAdapter = TaskAdapter(this@MainActivity)
 
 
-//        category_search_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//
-//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-//                val strCategory = category_search_spinner.selectedItem.toString()
-//
-//                val category = mRealm.where(Category::class.java).equalTo("strCategory", strCategory).findFirst().toString()
-//                val categoryRealmResults =
-//                    mRealm.where(Task::class.java).equalTo("category", category).findAll()
-//                mTaskAdapter.taskList = mRealm.copyFromRealm(categoryRealmResults)
-//
-//                // TaskのListView用のアダプタに渡す
-//                listViewTasks.adapter = mTaskAdapter
-//
-//                // 表示を更新するために、アダプターにデータが変更されたことを知らせる
-//                mTaskAdapter.notifyDataSetChanged()
-//            }
-//
-//            override fun onNothingSelected(parent: AdapterView<*>?) {
-//                reloadListView()
-//            }
-//
-//        }
+        category_search_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                categorySelected = mRealm.where(Category::class.java).equalTo("strCategory",category_search_spinner.selectedItem.toString()).findFirst()
+                reloadListView()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                reloadListView()
+            }
+
+        }
 
         //Intent MainActivity -> EditActivity
         listViewTasks.setOnItemClickListener { parent, _, position, _ ->
@@ -117,13 +112,16 @@ class MainActivity : AppCompatActivity() {
         //　category spinner
 //        val categoryAdapter = CategoryAdapter(this)
         val categoryRealmResults = mRealm.where(Category::class.java).findAll()
-        val categoryList = ArrayList<String>()
+        val categoryArrayList = ArrayList<String>()
         for (i in 1 .. categoryRealmResults.size) {
-            categoryList.add(categoryRealmResults[i -1]?.strCategory.toString())
+            categoryArrayList.add(categoryRealmResults[i -1]?.strCategory.toString())
         }
 
-        val categoryAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, categoryList)
+        val categoryAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, categoryArrayList)
         category_search_spinner.adapter = categoryAdapter
+
+        categorySelected = mRealm.where(Category::class.java).equalTo("strCategory",category_search_spinner.selectedItem.toString()).findFirst()
+        reloadListView()
     }
 
     override fun onDestroy() {
@@ -133,11 +131,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun reloadListView() {
-        // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得
-        val taskRealmResults = mRealm.where(Task::class.java).findAll()
+        // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得 　*訂正：カテゴリ選択されたやつだけ
+        val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
+        val taskArrayList = ArrayList<Task>()
+        for (i in 0 until taskRealmResults.size - 1) {
+            if (taskRealmResults[i]?.category == categorySelected) {
+                taskArrayList.add(taskRealmResults[i]!!)
+            }
+        }
 
         // 上記の結果を、TaskList としてセットする
-        mTaskAdapter.taskList = mRealm.copyFromRealm(taskRealmResults)
+//        mTaskAdapter.taskList = mRealm.copyFromRealm(taskRealmResults)
+        mTaskAdapter.taskList = taskArrayList
 
         // TaskのListView用のアダプタに渡す
         listViewTasks.adapter = mTaskAdapter
